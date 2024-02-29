4f28785ccfd79565c4688f3309736f696cb1ae6a
priyank <priyank@example.com>
2024-02-29 22:53:49 +0400

    Setting up the project base

---
+++ index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const { logger } = require('note-down');
+
+logger.warn('TODO: A task runner for your git projects to execute jobs at regular intervals and generate reports / alerts / notifications / mails');
