36df6f02b380860094a73dc53433cda02c405946
priyank <priyank@example.com>
2024-09-01 10:44:12 +0400

    Improved retry behavior for react-query

--- src/App/App.js
+++ src/App/App.js
@@ -14,7 +14,35 @@ import { PageBody } from './PageBody/PageBody.js';
 import { PageFooter } from './PageFooter/PageFooter.js';
 import { PageWidgets } from './PageWidgets/PageWidgets.js';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+
+            // eg:
+            //     * Go offline and come back online
+            //     * Locking and unlocking the mobile phone, while the webpage is open
+            refetchOnReconnect: false,
+
+            // https://github.com/TanStack/query/issues/2927#issuecomment-974706069
+            // https://github.com/TanStack/query/issues/2179
+            networkMode: 'always',
+
+            retry: function (failureCount, error) {
+                const errorResponseStatus = error?.response?.status;
+
+                if (
+                    failureCount < 3 &&
+                    [502, 503, 504].includes(errorResponseStatus)
+                ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+});
 
 const App = function () {
     return (
